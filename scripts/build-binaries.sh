348260ba527bebca0af2a78eea2fcc3c63b6a6f3
Alexander Weidemann <walnuss0815@gmail.com>
2021-12-20 19:55:34 +0100

    Add mips

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -6,6 +6,7 @@ env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -o build/netclient-arm6
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o build/netclient-arm7 main.go
 env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o build/netclient-arm64 main.go
 env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "-s -w" -o build/netclient-mipsle main.go && upx build/netclient-mipsle
+env CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "-s -w" -o build/netclient-mips main.go && upx build/netclient-mips
 env CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o build/netclient-freebsd main.go
 env CGO_ENABLED=0 GOOS=freebsd GOARCH=arm GOARM=5 go build -o build/netclient-freebsd-arm5 main.go
 env CGO_ENABLED=0 GOOS=freebsd GOARCH=arm GOARM=6 go build -o build/netclient-freebsd-arm6 main.go
